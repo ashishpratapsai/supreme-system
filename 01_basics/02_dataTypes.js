c1dba7f7aa12499fb3597483b2d4b9ae66b4b0ba
Automate With Ashish <ashishprataps.ai@gmail.com>
2025-10-23 11:01:49 +0000

    dtatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,3 +1,24 @@
 "use strict"; // treat all js code as newer version
 
 // alert("hello world"); we are using  nodejs, not browser
+
+console.log("Ashish");
+
+let name = "Ashish"
+let age = 24
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+//bright
+//string => ""
+//boolean => true/false
+//null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //null
